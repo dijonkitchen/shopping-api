adeaf37ed132855b3ef363f4df113abd1e21883a
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-02 19:54:38 -0400

    Add spec for default delivery status

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  customer = Customer.create(first_name: 'Jon', last_name: 'Chen')
+  order = customer.orders.create
+
   it 'creates successfully' do
-    customer = Customer.create(first_name: 'Jon', last_name: 'Chen')
-    order = customer.orders.create
     expect(order).to be_truthy
+  end
+
+  it 'defaults status to waiting for delivery' do
+    expect(order.status).to eq(0)
   end
 end
