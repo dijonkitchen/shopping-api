ba59414733d0e3b30c2079c9759815c12bda1272
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-02 17:53:42 -0400

    Add spec for customers API endpoint

---
+++ spec/requests/customers_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Customers', type: :request do
+  describe 'GET /customers' do
+    it 'successful request' do
+      get '/api/customers'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /customers/:id' do
+    it 'successful request' do
+      customer = Customer.create(first_name: 'J', last_name: 'C')
+      get "/api/customers/#{customer.id}"
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['first_name']).to eq('J')
+    end
+  end
+end
