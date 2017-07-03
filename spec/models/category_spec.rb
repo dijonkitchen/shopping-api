a24b0113bfeed8521f362899449b7ebf659f57ef
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-03 16:11:18 -0400

    Add spec for categories having many products

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  category = Category.create(name: 'Games')
+
   it 'creates a category with a name' do
-    category = Category.create(name: 'Games')
     expect(category.name).to eq('Games')
+  end
+
+  it 'has many products' do
+    category.products.create(name: 'Bang!')
+    expect(category.products.last.name).to eq('Bang!')
   end
 end
