349a7fe257d4478ba53cc7363c6c6ef7ee7b0383
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-03 16:12:45 -0400

    Add associations between Categories and Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
+  has_many :category_products
+  has_many :categories, through: :category_products
 end
