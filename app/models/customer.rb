f4398006d9d70fedbb92bd45173b19756fb3b97e
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-05 09:49:36 -0400

    Add method for num purchased in a category

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,16 @@
 class Customer < ApplicationRecord
   has_many :orders
+
+  def num_purchased_in(category)
+    Product.joins(:categories).joins(:orders)
+           .where(
+             categories: {
+               id: category.id
+             },
+             orders: {
+               id: orders
+             }
+           )
+           .count
+  end
 end
