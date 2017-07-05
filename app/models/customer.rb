6f5c0d569f9e1d2522e90b7673f908a854035eec
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-05 16:01:47 -0400

    Add associations for customers to other tables
    For new query

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ApplicationRecord
   has_many :orders
+  has_many :order_products, through: :orders
+  has_many :products, through: :order_products
+  has_many :category_products, through: :products
+  has_many :categories, through: :category_products
 
   def num_purchased_in(category)
     Product.joins(:categories).joins(:orders)
