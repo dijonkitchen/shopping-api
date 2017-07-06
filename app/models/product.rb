896572c784e3923342034b10604b278ed67d2ebd
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-05 22:15:54 -0400

    Add sold method for products
    Beware of time at end of days when near end of days! That's why the +1
    day was added! There's mismatch between local time and UTC!

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_products
   has_many :category_products
   has_many :categories, through: :category_products
+
+  def sold(starting: Date.today - 1.year, ending: Date.today)
+    orders.where(created_at: starting.beginning_of_day..(ending+1.day).end_of_day)
+          .count
+  end
 end
