d3721e19958af803000121e591463eddfcb6fb2c
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-06 06:24:55 -0400

    Add keyword argument for sold period

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
 
-  def sold(starting: Date.today - 1.year, ending: Date.today)
+  def sold(per:, starting: Date.today - 1.year, ending: Date.today)
     # SELECT COUNT(orders.id), DATE(orders.created_at)
     # FROM orders
     # INNER JOIN order_products
@@ -16,8 +16,16 @@ class Product < ApplicationRecord
     #     BETWEEN DATE('2017-01-01')
     #       AND DATE('2017-07-30')
     # GROUP BY DATE(orders.created_at);
-    orders.where(created_at: starting.beginning_of_day..(ending+1.day).end_of_day)
-          .group('DATE(orders.created_at)')
-          .count
+    amounts = orders.where(created_at: starting.beginning_of_day..(ending+1.day).end_of_day)
+                       .group('DATE(orders.created_at)')
+                       .count
+    case per
+    when 'day'
+      amounts
+    when 'week'
+
+    when 'month'
+
+    end
   end
 end
