54e7d538299a3287f409b03523af3aec85e80054
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-07-06 09:28:06 -0400

    Add spec for showing monthly sold for a product

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :request do
-  describe 'GET /products' do
+  describe 'shows sales breakdown by month for a date range' do
     it 'successful request' do
-      get '/api/products'
+      get '/api/products/1?per=month&starting=2016-12-31&ending=2017-06-30'
       expect(response).to have_http_status(200)
     end
   end
